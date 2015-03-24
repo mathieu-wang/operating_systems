e66b41cced74873faeb8e1300684ec2fa339426a
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-03-24 13:09:57 -0400

    Add skeleton and test code for simple file system

---
+++ simple_file_system/sfs.c
@@ -0,0 +1,29 @@
+#include "sfs_api.h"
+
+int mksfs(int fresh){
+	return 0;
+}
+int sfs_fopen(char *name) {
+	return 0;
+}
+int sfs_fclose(int fileID) {
+	return 0;
+}
+int sfs_fwrite(int fileID, const char *buf, int length) {
+	return 0;
+}
+int sfs_fread(int fileID, char *buf, int length) {
+	return 0;
+}
+int sfs_fseek(int fileID, int offset) {
+	return 0;
+}
+int sfs_remove(char *file) {
+	return 0;
+}
+int sfs_get_next_filename(char* filename) {
+	return 0;
+}
+int sfs_GetFileSize(const char* path) {
+	return 0;
+}
