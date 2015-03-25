81155cde6f46b5e918cb6f3557dd4e5378236a13
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-03-25 00:08:55 -0400

    Add superblock and inode structs

--- simple_file_system/sfs.c
+++ simple_file_system/sfs.c
@@ -1,6 +1,45 @@
 #include "sfs_api.h"
+#include "disk_emu.h"
+
+#define MAGIC_NUMBER 0xAABB0005
+#define BLOCK_SIZE 512
+#define NUM_BLOCKS 1000
+#define NUM_INODES 100
+
+//inode modes
+#define FILE 0
+#define DIR 1
+
+#define DEFAULT_UID 1
+#define DEFAULT_GID 1
+
+#define MAX_FNAME_LENGTH 20
+#define MAX_NUM_FILES 150
+#define OPEN_FILES_LIMIT 150
+
+//Struct and list definitions
+typedef struct superBlock {
+	//use long for everything because need minimum 4 bytes long
+	long magicNumber;// = MAGIC_NUMBER;
+	long blockSize;// = BLOCK_SIZE;
+	long fsSize;// = NUM_BLOCKS;
+	long inodeTableLength;// = NUM_INODES;
+	long rootDir;
+	long unused;
+} superBlock;
+
+// does not implement atime, ctime, mtime, access mode and indirect blocks
+typedef struct inode {
+	int mode;
+	int size;
+	int blockCount;
+	int uid;
+	int gid;
+	int blockPtrs[12];
+} inode;
 
 int mksfs(int fresh){
+	char* disk_name = "Disk";
 	return 0;
 }
 int sfs_fopen(char *name) {
